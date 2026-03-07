087fc9cd4230f42597910094f7a60ae49f769205
灿烂 <1481086138@qq.com>
2026-03-07 18:28:00 +0800

    fix: 关闭 react-in-jsx-scope 规则

--- eslint.config.js
+++ eslint.config.js
@@ -6,8 +6,14 @@ import { defineConfig } from "eslint/config";
 import prettierConfig from "eslint-config-prettier";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',          // ← 加这行，关闭规则
+      'react/jsx-uses-react': 'off',              // ← 可选，也关掉（旧规则）
+    },
+  },
   prettierConfig
 ]);
