608dcaed0c2e5ecd7cd46ee4e2c72a62f1474a20
灿烂 <1481086138@qq.com>
2026-03-14 17:30:54 +0800

    feat: 配置eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,38 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-import prettierConfig from "eslint-config-prettier";
+import js from "@eslint/js"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import pluginReact from "eslint-plugin-react"
+import { defineConfig } from "eslint/config"
+import prettierConfig from "eslint-config-prettier"
+import reactHooks from "eslint-plugin-react-hooks"
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } }
+  },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  },
+  {
+    plugins:{
+      "react-hooks":reactHooks
+    }
+  },
+  {
     rules: {
-      'react/react-in-jsx-scope': 'off',          // ← 加这行，关闭规则
-      'react/jsx-uses-react': 'off',              // ← 可选，也关掉（旧规则）
-    },
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      ...reactHooks.configs.recommended.rules
+    }
   },
   prettierConfig
-]);
+])
