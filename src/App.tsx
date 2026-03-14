2122e5e257edb65ad19dac23572266387c327173
灿烂 <1481086138@qq.com>
2026-03-14 19:34:44 +0800

    feat: 完成CTA部分

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Layout } from "./components/Layout"
 import About from "./components/sections/About"
 import Brands from "./components/sections/Brands"
+import CTA from "./components/sections/CallToAction"
 import Hero from "./components/sections/Hero"
 import Price from "./components/sections/Price"
 import Service from "./components/sections/Service"
@@ -13,6 +14,7 @@ export default function App() {
     <Service />
     <About />
     <Price />
+    <CTA />
     <div className="w-full h-48 bg-violet-500"></div>
   </Layout>
 }
---
+++ src/components/sections/CallToAction.tsx
@@ -0,0 +1,22 @@
+import Container from "../shared/Container"
+import Paragraph from "../shared/Paragraph"
+
+export default function CTA() {
+  return (
+    <section id="cta">
+      <Container>
+        <div className="text-center">
+          <h1 className="text-6xl font-bold text-heading-1 mb-4">
+            Quick Start you <span className="bg-clip-text text-transparent bg-linear-to-r from-indigo-600 to-violet-600">own AI</span> Business
+          </h1>
+          <Paragraph className="my-12">
+            Leverage our AI-powered tools and expert support to quickly launch and grow your AI-driven business. To startup or an established company, we provide the resources and guidance you need to succeed in the AI landscape.
+          </Paragraph>
+          <button className="bg-violet-600 hover:bg-violet-700 text-heading-1 font-bold py-3 px-6 rounded-full cursor-pointer transition-colors duration-300">
+            Get In Touch
+          </button>
+        </div>
+      </Container>
+    </section>
+  )
+}
