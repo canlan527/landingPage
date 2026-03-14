b2cc3cbbf377505dfc260dd0c5c211b33524e2be
灿烂 <1481086138@qq.com>
2026-03-14 20:00:46 +0800

    fix: 修改关于卡片样式

--- src/components/cards/AboutCard.tsx
+++ src/components/cards/AboutCard.tsx
@@ -9,7 +9,7 @@ interface AboutCardProps {
 export default function AboutCard({ title, description, children }: AboutCardProps) {
   return (
     <div className="border rounded-3xl bg-box-bg p-8 sm:p-6 lg:p-8">
-      <div className="my-4 rounded-xl bg-gray dark:bg-gray-950 p-3 text-heading-1 w-max relative">{children}</div>
+      <div className="my-4 rounded-xl bg-body p-3 text-heading-1 w-max relative">{children}</div>
       <h1 className="text-heading-2 w-max relative md:text-xl font-bold">{title}</h1>
       <Paragraph>{description}</Paragraph>
     </div>
